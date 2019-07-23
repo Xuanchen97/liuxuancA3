1e0d947c9bc499132e6e80ff6232c41f110e5bed
Xuanchen Liu <45085738+Xuanchen97@users.noreply.github.com>
2019-07-23 15:13:25 -0400

    Add files via upload

--- assets/data/myBooks.ts
+++ assets/data/myBooks.ts
@@ -1,11 +1,11 @@
 import {books} from '../../app/bookInfo';
 export const mybooks : books[] = 
 [
-  {bname : 'Atomic Habits', author: 'James Clear', bgenre: 'Education'
+  {bname : 'Atomic', author: 'James Clear', bgenre: 'Education'
   ,ypbulished : '2013',picture :'1.jpg'},
-  {bname : 'When Breath Becomes Air', author: 'Paul Kalanithi',
+  {bname : 'Breath Becomes Air', author: 'Paul Kalanithi',
    bgenre: 'Novel',ypbulished : '2011',picture :'2.jpg'},
-  {bname : 'Sapiens: A Brief History of Humankind', author: 'Yuval Noah Harari', 
+  {bname : 'Sapiens', author: 'Yuval Noah Harari', 
   bgenre: 'Biography',ypbulished : '2015',picture :'3.jpg'},
   {bname : 'Guns, Germs, and Steel', author: 'Jared Diamond', 
   bgenre: 'Biography',ypbulished : '2017',picture :'4.jpg'}
